a6fa24836c3f06e1b7aed4fdac1bf896a454b63e
EugenioAloi <60814811+eugenioaloi@users.noreply.github.com>
2024-01-30 12:08:15 +0100

    old project deledeted

--- Booking_portal/src/main/java/com/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.entity.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
